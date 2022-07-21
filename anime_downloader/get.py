df288f3cad24f59ad0c027e258b7440ac415d1fc
Yohan Min <minleeyohan@gmail.com>
2022-07-20 19:35:13 -0500

    Change XPath of video iframe

--- anime_downloader/get.py
+++ anime_downloader/get.py
@@ -237,7 +237,7 @@ def get_anime_data(*args, driver=None):
 	for url in args:
 		driver.get(url)
 
-		xpath = "//div[@id='movie_player']//iframe"
+		xpath = "//div[@class='view-padding']//iframe"
 		source = WebDriverWait(driver, CONFIG['wait']).until(
 			EC.presence_of_element_located((By.XPATH, xpath))
 		).get_attribute("src")
