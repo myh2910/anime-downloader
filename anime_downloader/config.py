bcfe41829fb88f47c9c2b656667b10c32be9c10b
Yohan Min <minleeyohan@gmail.com>
2022-07-22 15:20:20 -0500

    Change CONFIG

--- anime_downloader/config.py
+++ anime_downloader/config.py
@@ -4,15 +4,14 @@ The configuration file for the downloader.
 """
 CONFIG = {
 	'headers': {
-		'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) \
-Gecko/20100101 Firefox/98.0",
+		'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
 		'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
 		'X-Requested-With': "XMLHttpRequest",
 		'Cookie': "fireplayer=5ia1hca1n8070rr90j57ip73eb"
 	},
 	'quality': "best",
 	'ext': ".mkv",
-	'home': "anime",
+	'home': "Anime",
 	'wait': 20,
 	'show_rows': 50,
 	'minimize': False,
