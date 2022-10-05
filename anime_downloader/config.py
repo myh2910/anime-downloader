d6fb86ee77c50737570b17c23b1ec0d716150d0f
Yohan Min <minleeyohan@gmail.com>
2022-10-04 19:10:35 -0500

    Update my User Agent

--- anime_downloader/config.py
+++ anime_downloader/config.py
@@ -4,7 +4,7 @@ The configuration file for the downloader.
 """
 CONFIG = {
 	'headers': {
-		'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+		'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
 		'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
 		'X-Requested-With': "XMLHttpRequest",
 		'Cookie': "fireplayer=5ia1hca1n8070rr90j57ip73eb"
