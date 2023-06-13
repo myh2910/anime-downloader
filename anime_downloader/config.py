27b23b73a6bc34275bf2c97feb4cad8484774906
Yohan Min <minleeyohan@gmail.com>
2023-06-13 12:28:25 +0900

    Update User-Agent

--- anime_downloader/config.py
+++ anime_downloader/config.py
@@ -1,6 +1,6 @@
 CONFIG = {
     "headers": {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "X-Requested-With": "XMLHttpRequest",
         "Cookie": "fireplayer=5ia1hca1n8070rr90j57ip73eb",
